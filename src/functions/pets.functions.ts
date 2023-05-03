2ffe268b1957d52273276b5e57e14803b9ad71ee
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-05-03 23:02:02 +0100

    Re-factored

--- src/functions/pets.functions.ts
+++ src/functions/pets.functions.ts
@@ -1,11 +1,57 @@
-import db from '../database/database';
+// import db from '../database/database';
+import {writeFile, readFile} from 'fs'
 
-export const getItem = (id: number) => {
+async function writeData(filename: string, obj: any): Promise<void> {
     try {
-        if (!db || !db.pets) {
+      const newObj = JSON.stringify(obj);
+      await writeFileAsync(filename, newObj);
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+function writeFileAsync(filename: string, data: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+        writeFile(filename, data, (err) => {
+        if (err) reject(err);
+        else resolve();
+        });
+    });
+}
+
+
+async function readData(filename: string): Promise<[{
+    id: number;
+    name: string;
+    type: string;
+    age: number;
+    breed: string;
+}]> {
+    try {
+      const data = await readFileAsync(filename);
+      const parsedData = JSON.parse(data);
+      return parsedData;
+    } catch (error) {
+      throw error;
+    }
+}
+
+function readFileAsync(filename: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      readFile(filename, 'utf8', (err, data) => {
+        if (err) reject(err);
+        else resolve(data);
+      });
+    });
+  }
+
+export async function getItem (id: number) {
+    try {
+        const db = await readData('./pets.txt')
+        if (!db) {
             return("Database or pets array does not exist.")
         }
-        const pet = db.pets.filter(pet => pet?.id === id)[0]
+        const pet = db.filter(pet => pet?.id === id)[0]
         if (!pet) {
             return("Pet with given ID does not exist.")
         }
@@ -15,68 +61,75 @@ export const getItem = (id: number) => {
     }
 };
 
-export const addItem = (data: {
+export async function addItem (data: {
     name: string;
     type: string;
     age: number;
     breed: string;
-}) => {
+}) {
     try {
-        if (!db || !db.pets) {
+        const db = await readData('./pets.txt')
+        if (!db) {
             return("Database or pets array does not exist.")
         }
-        const newPet = { id: db.pets.length + 1, ...data }
-        db.pets.push(newPet)
+        const newPet = { id: db.length + 1, ...data }
+        db.push(newPet)
+        await writeData('./pets.txt', newPet)
         return newPet
     } catch (error: any) {
         throw new Error(`An error occurred, ${error.message}`)
     }
 };
 
-export const listItems = () => {
+export async function listItems () {
     try {
-        if (!db || !db.pets) {
+        const db = await readData('./pets.txt')
+        if (!db) {
             return("Database or pets array does not exist.")
         }
-        return db.pets
+        return db
     } catch (error: any) {
         throw new Error(`An error occurred, ${error.message}`)
     }
 };
 
-export const deleteItem = (id: number) => {
+export async function deleteItem (id: number) {
     try {
-        if (!db || !db.pets) {
+        const db = await readData('./pets.txt')
+        if (!db) {
             return("Database or pets array does not exist.")
         }
-        const index = db.pets.findIndex(pet => pet.id === id)
+        const index = db.findIndex(pet => pet.id === id)
         if (index === -1) {
             return('Pet not found')
         } else {
-            db.pets.splice(index, 1)
-            console.log(db.pets)
-            return db.pets
+            db.splice(index, 1)
+            await writeData('./pets.text', db)
+            console.log(db)
+            return db
         }
     } catch (error: any) {
         throw new Error(`An error occurred, ${error.message}`)
     }
 };
 
-export const updateItem = (id: number, data: {
+export async function updateItem (id: number, data: {
     name: string;
     type: string;
     age: number;
     breed: string;
-}) => {
+}) {
     try {
-        const index = db.pets.findIndex(pet => pet.id === id)
+        const db = await readData('./pets.txt')
+        const index = db.findIndex(pet => pet.id === id)
         if (index === -1) {
             return('Pet not found')
         } else {
             const dataWithId = { id: index, ...data }
-            db.pets[index] = dataWithId
-            console.log(db.pets)
-            return db.pets[index]
+            db[index] = dataWithId
+            writeData('./pets.text', db)
+            console.log(db)
+            return db
         }
     } catch (error: any) {
         throw new Error(`An error occurred, ${error.message}`)
