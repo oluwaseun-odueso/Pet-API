3972690671cf011931c8904a538a8e1313a66edf
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-05-03 01:08:00 +0100

    Server set up

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, {Request, Response} from 'express';
+import dotenv from 'dotenv';
+const petRoutes = require('./routes/pets')
+
+dotenv.config();
+const port = process.env.PORT || 3000
+
+const app = express();
+
+app.use('/pets', petRoutes);
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({success: true, message: "Welcome to the official pet homepage"})
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
+
+module.exports = app;
