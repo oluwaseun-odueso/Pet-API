9b3e583a1a2f85e7e9cecd18e5a7409634a6c4a0
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-05-04 02:39:49 +0100

    WIP

--- src/functions/pets.functions.ts
+++ src/functions/pets.functions.ts
@@ -1,109 +1,254 @@
 // import db from '../database/database';
-import {writeFile, readFile} from 'fs'
+import {writeFile, readFile} from 'fs/promises'
 
-async function writeData(filename: string, data: any): Promise<void> {
-    try {
-      const newData = JSON.stringify(data);
-      await writeFileAsync(filename, newData);
-    } catch (error) {
-      throw error;
-    }
-  }
+// // async function writeData(filename: string, data: any): Promise<void> {
+// //     try {
+// //       const newData = JSON.stringify(data);
+// //       await writeFileAsync(filename, newData);
+// //     } catch (error) {
+// //       throw error;
+// //     }
+// //   }
   
-function writeFileAsync(filename: string, data: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-        writeFile(filename, data, (err) => {
-        if (err) reject(err);
-        else resolve();
-        });
-    });
-};
+// // function writeFileAsync(filename: string, data: string): Promise<void> {
+// //     return new Promise((resolve, reject) => {
+// //         writeFile(filename, data, (err) => {
+// //         if (err) reject(err);
+// //         else resolve();
+// //         });
+// //     });
+// // };
 
-async function readData(filename: string): Promise<[{
-    id: number;
-    name: string;
-    type: string;
-    age: number;
-    breed: string;
-}]> {
+// // async function readData(filename: string): Promise<[{
+//     // id: number;
+//     // name: string;
+//     // type: string;
+//     // age: number;
+//     // breed: string;
+// // }]> {
+// //     try {
+// //       const data = await readFileAsync(filename);
+// //       const parsedData = JSON.parse(data);
+// //       return parsedData;
+// //     } catch (error) {
+// //       throw error;
+// //     }
+// // }
+
+// // function readFileAsync(filename: string): Promise<string> {
+// //     return new Promise((resolve, reject) => {
+// //       readFile(filename, 'utf8', (err, data) => {
+// //         if (err) reject(err);
+// //         else resolve(data);
+// //       });
+// //     });
+// //   }
+
+interface PetInterface {
+    id?: number,
+    name: string,
+    type: string,
+    age: number,
+    breed: number
+}
+
+// function readData (filename: string): {
+//     readFile(filename, 'utf8', (error, data) => {
+//         if (error) throw error
+
+//         const petData = JSON.parse(data);
+//         console.log(petData)
+//         return petData
+//     })
+// }
+
+// function writeData (filename: string, data: PetInterface) {
+//     const newPet = JSON.stringify(data)
+//     writeFile(filename, newPet, (error) => {
+//         if (error) throw error;
+//     })
+// }
+
+// // export async function addItem (data: PetInterface) {
+// //     try {
+// //         const db: any = readData('./src/pets.txt')
+// //         if (!db) {
+// //             const arrayObj: PetInterface[] = []
+// //             arrayObj.push({ id: 1, ...data })
+// //             console.log(arrayObj)
+// //             writeData('./src/pets.txt', arrayObj)
+// //             return arrayObj;
+// //         } else {
+// //             // const newPet = { id: db.length + 1, ...data }
+// //             db.push({ id: db.length + 1, ...data })
+// //             writeData('./src/pets.txt', db)
+// //             return db;
+// //         }
+// //     } catch (error: any) {
+// //         throw new Error(`An error occurred, ${error.message}`)
+// //     }
+// // };
+// export async function addItem(data: PetInterface) {
+//     try {
+//         const db = await readData('./src/pets.txt');
+//         if (db !== null && db !== undefined) {
+//             db.push({ id: db.length + 1, ...data });
+//             writeData('./src/pets.txt', db);
+//             return db;
+//         } else {
+//             const arrayObj: PetInterface[] = [{ id: 1, ...data }];
+//             writeData('./src/pets.txt', arrayObj);
+//             return arrayObj;
+//         }
+//     } catch (error: any) {
+//         throw new Error(`An error occurred, ${error.message}`);
+//     }
+// };
+
+
+// export async function getItem (id: number) {
+//     try {
+//         const db = readData('./src/pets.txt')
+//         if (!db) {
+//             return("Database or pets array does not exist.")
+//         }
+//         const pet = db.filter(pet => pet?.id === id)[0]
+//         if (!pet) {
+//             return("Pet with given ID does not exist.")
+//         }
+//         return pet
+//     } catch (error: any) {
+//         throw new Error(`An error occurred, ${error.message}`)
+//     }
+// };
+
+// export async function listItems () {
+//     try {
+//         const db = readData('./src/pets.txt')
+//         if (!db) {
+//             return("Database or pets array does not exist.")
+//         }
+//         return db
+//     } catch (error: any) {
+//         throw new Error(`An error occurred, ${error.message}`)
+//     }
+// };
+
+// export async function deleteItem (id: number) {
+//     try {
+//         const db = readData('./src/pets.txt')
+//         if (!db) {
+//             return("Database or pets array does not exist.")
+//         }
+//         const index = db.findIndex(pet => pet.id === id)
+//         if (index === -1) {
+//             return('Pet not found')
+//         } else {
+//             db.splice(index, 1)
+//             writeData('./src/pets.text', db)
+//             console.log(db)
+//             return db
+//         }
+//     } catch (error: any) {
+//         throw new Error(`An error occurred, ${error.message}`)
+//     }
+// };
+
+// export async function updateItem (id: number, data: {
+//     name: string;
+//     type: string;
+//     age: number;
+//     breed: string;
+// }) {
+//     try {
+//         const db = readData('./src/pets.txt')
+//         const index = db.findIndex(pet => pet.id === id)
+//         if (index === -1) {
+//             return('Pet not found')
+//         } else {
+//             const dataWithId = { id: index, ...data }
+//             db[index] = dataWithId
+//             writeData('./pets.text', db)
+//             console.log(db)
+//             return db
+//         }
+//     } catch (error: any) {
+//         throw new Error(`An error occurred, ${error.message}`)
+//     }
+// }
+
+
+
+async function readData(filename: string): Promise<PetInterface[]> {
     try {
-      const data = await readFileAsync(filename);
-      const parsedData = JSON.parse(data);
-      return parsedData;
+        const data = await readFile(filename, 'utf8');
+        return JSON.parse(data);
     } catch (error) {
-      throw error;
+        throw error;
     }
 }
 
-function readFileAsync(filename: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-      readFile(filename, 'utf8', (err, data) => {
-        if (err) reject(err);
-        else resolve(data);
-      });
-    });
-  }
-
-export async function addItem (data: {
-    name: string;
-    type: string;
-    age: number;
-    breed: string;
-}) {
+async function writeData(filename: string, data: PetInterface[]): Promise<void> {
     try {
-        const db = await readData('./src/pets.txt')
-        if (!db) {
-            const arrayObj: {}[] = []
-            arrayObj.push({ id: 1, ...data })
-            console.log(arrayObj)
-            await writeData('./src/pets.txt', arrayObj)
-            return arrayObj;
-        } else {
-            // const newPet = { id: db.length + 1, ...data }
-            db.push({ id: db.length + 1, ...data })
-            await writeData('./src/pets.txt', db)
+        const newData = JSON.stringify(data);
+        await writeFile(filename, newData);
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function addItem(data: PetInterface): Promise<PetInterface[]> {
+    try {
+        const db = await readData('./src/pets.txt');
+        if (db !== null && db !== undefined) {
+            db.push({ id: db.length + 1, ...data });
+            await writeData('./src/pets.txt', db);
             return db;
+        } else {
+            const arrayObj: PetInterface[] = [{ id: 1, ...data }];
+            await writeData('./src/pets.txt', arrayObj);
+            return arrayObj;
         }
     } catch (error: any) {
-        throw new Error(`An error occurred, ${error.message}`)
+        throw new Error(`An error occurred, ${error.message}`);
     }
 };
 
-export async function getItem (id: number) {
+export async function getItem(id: number): Promise<PetInterface | string> {
     try {
-        const db = await readData('./src/pets.txt')
-        if (!db) {
-            return("Database or pets array does not exist.")
+        const db = await readData('./src/pets.txt');
+        if (db === null || db === undefined) {
+            return("Database or pets array does not exist.");
         }
-        const pet = db.filter(pet => pet?.id === id)[0]
+        const pet = db.filter(pet => pet?.id === id)[0];
         if (!pet) {
-            return("Pet with given ID does not exist.")
+            return("Pet with given ID does not exist.");
         }
-        return pet
+        return pet;
     } catch (error: any) {
-        throw new Error(`An error occurred, ${error.message}`)
+        throw new Error(`An error occurred, ${error.message}`);
     }
 };
 
-export async function listItems () {
+export async function listItems(): Promise<PetInterface[] | string> {
     try {
-        const db = await readData('./src/pets.txt')
-        if (!db) {
-            return("Database or pets array does not exist.")
+        const db = await readData('./src/pets.txt');
+        if (db === null || db === undefined) {
+            return("Database or pets array does not exist.");
         }
-        return db
+        return db;
     } catch (error: any) {
-        throw new Error(`An error occurred, ${error.message}`)
+        throw new Error(`An error occurred, ${error.message}`);
     }
 };
 
-export async function deleteItem (id: number) {
+export async function deleteItem(id: number): Promise<PetInterface[] | string> {
     try {
-        const db = await readData('./src/pets.txt')
-        if (!db) {
-            return("Database or pets array does not exist.")
+        const db = await readData('./src/pets.txt');
+        if (db === null || db === undefined) {
+            return("Database or pets array does not exist.");
         }
-        const index = db.findIndex(pet => pet.id === id)
+        const index = db.findIndex(pet => pet.id === id);
         if (index === -1) {
             return('Pet not found')
         } else {
@@ -117,21 +262,19 @@ export async function deleteItem (id: number) {
     }
 };
 
-export async function updateItem (id: number, data: {
-    name: string;
-    type: string;
-    age: number;
-    breed: string;
-}) {
+export async function updateItem (id: number, data: PetInterface): Promise<PetInterface[] | string> {
     try {
-        const db = await readData('./src/pets.txt')
+        const db = await readData('./src/pets.txt');
+        if (db === null || db === undefined) {
+            return("Database or pets array does not exist.");
+        }
         const index = db.findIndex(pet => pet.id === id)
         if (index === -1) {
             return('Pet not found')
         } else {
             const dataWithId = { id: index, ...data }
             db[index] = dataWithId
-            writeData('./pets.text', db)
+            await writeData('./pets.text', db)
             console.log(db)
             return db
         }
@@ -139,3 +282,4 @@ export async function updateItem (id: number, data: {
         throw new Error(`An error occurred, ${error.message}`)
     }
 }
+
